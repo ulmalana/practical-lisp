3190a940ede351258bec1f34b86ae4e2206efe2a
RM <muhamaul@gmail.com>
2022-06-01 15:52:05 +0700

    [LISP] started chapter 6

---
+++ ch06/let.lisp
@@ -0,0 +1,9 @@
+;;;; variables binding and shadowing
+(defun foo (x)
+  (format t "Parameter: ~a~%" x)
+  (let ((x 2))
+    (format t "Outer LET: ~a~%" x)
+    (let ((x 3))
+      (format t "Inner LET: ~a~%" x))
+    (format t "Outer LET: ~a~%" x))
+  (format t "Parameter: ~a~%" x))
